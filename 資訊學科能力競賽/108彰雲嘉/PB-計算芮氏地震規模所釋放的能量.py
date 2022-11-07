a57152843afcaa86159d7e31beea8f156944b8e5
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-07 14:11:00 +0800

    Update PB-計算芮氏地震規模所釋放的能量.py

--- 資訊學科能力競賽/108彰雲嘉/PB-計算芮氏地震規模所釋放的能量.py
+++ 資訊學科能力競賽/108彰雲嘉/PB-計算芮氏地震規模所釋放的能量.py
@@ -1,6 +1,6 @@
-n=int(input())
-TNT=4184000
-for i in range(n):
-    d=float(input())
-    E=10**(d*1.5+4.8)
-    print(f'{E/TNT:.1f}')
+n=int(input())  #輸入n
+TNT=4184000  #定義TNT
+for i in range(n):  #做n次
+    d=float(input())  #輸入芮氏規模
+    E=10**(d*1.5+4.8)  #計算E(能量)
+    print(f'{E/TNT:.1f}')  #輸出E/TNT並取到小數點後1位
