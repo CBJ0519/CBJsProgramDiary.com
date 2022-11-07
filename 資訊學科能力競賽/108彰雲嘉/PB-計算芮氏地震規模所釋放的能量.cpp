2a7c73ceec2e4344c8ef5e51d09d9be94c009b86
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-07 11:45:34 +0800

    Update PB-計算芮氏地震規模所釋放的能量.cpp

--- 資訊學科能力競賽/108彰雲嘉/PB-計算芮氏地震規模所釋放的能量.cpp
+++ 資訊學科能力競賽/108彰雲嘉/PB-計算芮氏地震規模所釋放的能量.cpp
@@ -1,14 +1,12 @@
-#include<bits/stdc++.h>
-#define int long long
-#define double long double
+#include<iostream>
+#include<iomanip>
+#include<cmath>
 #define TNT 4184000
 using namespace std;
 double trans(double d){
     return pow(10,d*1.5+4.8);
 }
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
+int main(){
     int n;cin>>n;
     while(n--){
         double d;
@@ -18,12 +16,3 @@ signed main(){
     }
     return 0;
 }
-/*
-6
-1.0
-3.0
-5.4
-7.9
-9.1
-10.0
-*/
