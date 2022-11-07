e05b97569a56d29b5135898114f9d35afa239221
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-07 11:40:18 +0800

    Update PA-通用商品碼校驗碼產生器.cpp

--- 資訊學科能力競賽/108彰雲嘉/PA-通用商品碼校驗碼產生器.cpp
+++ 資訊學科能力競賽/108彰雲嘉/PA-通用商品碼校驗碼產生器.cpp
@@ -1,9 +1,8 @@
-#include<bits/stdc++.h>
-#define int long long
+#include<iostream>
+#include<string>
+#include<cctype>
 using namespace std;
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
+int main(){
     int n;
     cin>>n;
     while(n--){
