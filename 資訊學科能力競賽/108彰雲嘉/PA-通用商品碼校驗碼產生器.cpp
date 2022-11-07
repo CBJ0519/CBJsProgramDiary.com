36ce7997b48dd27b00a446086fb38e8c4cca23df
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-07 11:46:38 +0800

    Update PA-通用商品碼校驗碼產生器.cpp

--- 資訊學科能力競賽/108彰雲嘉/PA-通用商品碼校驗碼產生器.cpp
+++ 資訊學科能力競賽/108彰雲嘉/PA-通用商品碼校驗碼產生器.cpp
@@ -1,32 +1,39 @@
 #include<iostream>
-#include<string>
-#include<cctype>
+#include<string>  //內含string
+#include<cctype>  //內含isdigit()(判斷字串是否為數字)
 using namespace std;
 int main(){
     int n;
-    cin>>n;
-    while(n--){
-        string s;
-        cin>>s;
-        if(s.length()!=11){
+ 
+    cin>>n;  //讀入n
+    while(n--){  //做n次
+        string s;  
+        cin>>s;   //輸入字串
+ 
+        if(s.length()!=11){  //例外處理(長度不為11)
             cout<<-1<<"\n";
-            return 0;
+            return 0;  //結束程式
         }
-        int total=0;
-        for(int i=0;i<s.length();i++){
-            if(!isdigit(s[i])){
+ 
+        int total=0;  //總和
+        for(int i=0;i<s.length();i++){  //走訪s
+ 
+            if(!isdigit(s[i])){  //例外處理(不是數字)
                 cout<<-1<<"\n";
-                return 0;
+                return 0;  //結束程式
             }
-            if(i%2){
-                total+=s[i]-'0';
+ 
+            if(i%2){  //假如 i%2 == 1(true),則為奇數位
+                total+=s[i]-'0';  //s[i]轉整數後加入total
             }
-            else{
-                total+=(s[i]-'0')*3;
+            else{  //否則為偶數位
+                total+=(s[i]-'0')*3;  //s[i]轉整數後*3加入total
             }
         }
-        int ans=10-(total%10);
-        cout<<ans<<"\n";
+ 
+        int ans=10-(total%10);  //任何數%10可得該數的個位數,將total%10再被10減即為所求
+ 
+        cout<<ans<<"\n";   //輸出ans(校驗碼)
     }
     return 0;
 }
