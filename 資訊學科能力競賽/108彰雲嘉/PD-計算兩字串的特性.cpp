5b84ad3d46fb1f5709dde6f6cc06bb1bffad08a4
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-07 15:25:34 +0800

    Update PD-計算兩字串的特性.cpp

--- 資訊學科能力競賽/108彰雲嘉/PD-計算兩字串的特性.cpp
+++ 資訊學科能力競賽/108彰雲嘉/PD-計算兩字串的特性.cpp
@@ -1,24 +1,28 @@
-#include<bits/stdc++.h>
-#define int long long
+#include<iostream>
+#include<string>  //內含string
+#include<vector>  //內含vector
+#include<algorithm>  //內含find()(用於尋找的函數)
 using namespace std;
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
+
+int main(){
     string a,b;
     cin>>a>>b;
-    vector<char>in_b;
-    for(char c:b)in_b.push_back(c);
+    vector<char>in_b;   
+    for(char c:b)in_b.push_back(c);  //將b的所有內容丟入in_b
+    
     int Jia=0,Yi=0;
-    for(int i=0;i<a.length();i++){
-        if(a[i]==b[i]){
-            Jia++;
-            continue;
+    for(int i=0;i<a.length();i++){   //遍歷a
+        if(a[i]==b[i]){  //假如同個位置有同個值
+            Jia++;   //更新甲
+            continue;  //跳過檢查乙特性
         }
-        auto target=find(in_b.begin(),in_b.end(),a[i]);
-        if(target!=in_b.end()){
-            Yi++;
+        
+        auto target=find(in_b.begin(),in_b.end(),a[i]);  //找尋a[i]是否有在b裡
+        if(target!=in_b.end()){  //若回傳值不是in_b.end(),則表示有找到
+            Yi++;  //更新乙
         }
     }
-    cout<<Jia<<" "<<Yi<<"\n";
+    
+    cout<<Jia<<" "<<Yi<<"\n";  //輸出甲,乙
     return 0;
 }
