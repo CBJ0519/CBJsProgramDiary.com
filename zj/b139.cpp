763f7e862ad8385e7293ba8d1c5159d20a891481
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-07 12:22:18 +0800

    Update b139.cpp

--- zj/b139.cpp
+++ zj/b139.cpp
@@ -1,5 +1,7 @@
+//C++ language
 #include<iostream>
 #include<algorithm>  //內含fill()(填充函式，可填1)
+#include<numeric>  //內含accumulate()(計算陣列總和)
 using namespace std;
 int main(){
     int L,M,a[10005];
@@ -12,8 +14,7 @@ int main(){
         for(int i=start;i<=end;i++) a[i]=0;
     }
     int sum=0;
-    //計算總和
-    for(int i=0;i<=L;i++) sum+=a[i];
-    cout<<sum<<"\n";
+    //計算總和並輸出
+    cout<<accumulate(a,a+L+1,0)<<"\n";
     return 0;
 }
