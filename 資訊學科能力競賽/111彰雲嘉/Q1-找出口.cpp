8b0c652e5d812cb72e8ead2fcb988edc3db698d2
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-11 23:03:34 +0800

    Update Q1-找出口.cpp

--- 資訊學科能力競賽/111彰雲嘉/Q1-找出口.cpp
+++ 資訊學科能力競賽/111彰雲嘉/Q1-找出口.cpp
@@ -1,31 +1,31 @@
-#include<bits/stdc++.h>
-#define int long long
+#include<iostream>
 using namespace std;
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
 
-    int x;
-    cin>>x;
-
+int main(){
+    int x; cin>>x;  //讀入出口位置
+    
+    //預先判斷x是否為負
     bool isneg=false;
     if(x<0) isneg=true;
-
-    int now=0;
-    int base=1;
-    int dir=1;
-    int times=0;
-    while(1){
-        if((isneg && now<=x) || (!isneg && now>=x)){
+    
+    int now=0;   //現在位置
+    int base=1;  //要走base*base步(base每次++)
+    int dir=1;   //往左或往右(往左就要乘上-1)
+    int times=0; //花費次數
+    
+    while(1){   //跑無窮迴圈
+        
+        //假如找到出口(x為負 && 當前位置 < x) || (x為正 && 當前位置 > x)
+        if((isneg && now<=x) || (!isneg && now>=x)){   
             break;
         }
-        now+=base*base*dir;
-        base++;
-        dir*=-1;
-        times++;
+        
+        now += base*base*dir;  //更新now並乘上方向
+        base++;  //1 -> 2 -> 3 ...
+        dir*=-1;  //方向改變(1 -> -1, -1 -> 1)
+        times++;  //次數++
     }
-
-    cout<<times<<"\n";
-
+    
+    cout<<times<<"\n";  //輸出次數
     return 0;
 }
