1f763bebfdd48cb302463073aaa53e89497545f6
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-13 09:15:36 +0800

    Update Q7-字串編碼.cpp

--- 資訊學科能力競賽/111彰雲嘉/Q7-字串編碼.cpp
+++ 資訊學科能力競賽/111彰雲嘉/Q7-字串編碼.cpp
@@ -1,24 +1,28 @@
-#include<bits/stdc++.h>
-#define int long long
+#include<iostream>
+#include<string>  //內含string
 using namespace std;
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
 
+int main(){
     string s;
-    getline(cin,s);
-
-    char now=s[0];
-    int cnt=1;
+    getline(cin,s);  //因為輸入可能含有空白，所以用getline()讀
+    
+    char last=s[0];  //前一個字元
+    int cnt=1;  //前一個字元出現次數
+    
+    //遍歷s
     for(int i=1;i<s.length();i++){
-        if(s[i]==now) cnt++;
+        //假如當前字元等於前一個字元
+        if(s[i]==last) cnt++;
         else{
-            cout<<cnt<<now;
-            now=s[i];
+            //先輸出前面的統計結果
+            cout<<cnt<<last;
+            
+            //將前一個字元改成當前字元，並將cnt歸回1(因為現在這個也要算)
+            last=s[i];
             cnt=1;
         }
     }
-    cout<<cnt<<now<<"\n";
-
+    
+    cout<<cnt<<last<<"\n";  //輸出最後沒被輸出的統計
     return 0;
 }
