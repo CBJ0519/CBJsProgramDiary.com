6ffb266d56dae6a7b89c0285595155a85b6890d5
CBJ0519 <104713329+CBJ0519@users.noreply.github.com>
2022-11-17 20:54:00 +0800

    Update 11332.c

--- CPE/11332.c
+++ CPE/11332.c
@@ -11,7 +11,7 @@ int len(char s[11]){  //計算字串長度
     return cnt;
 }
 
-void sum(char s[11]){
+void sum(char s[11]){  //計算字串總和並轉成字串回傳
     int ans=0;
     for(int i=0;isdigit(s[i]);i++){  //假如出現非數值，則結束for迴圈
         ans+=(s[i]-'0');  //字元轉數值技巧(運用ASCII)
@@ -19,7 +19,7 @@ void sum(char s[11]){
     sprintf(ret,"%d",ans);  //將整數ans轉成字串並用ret[]儲存
 }
 
-char* f(char s[11]){
+char* f(char s[11]){  //題目中的f()
     if(len(s)==1) return s;  //題目所述的終止條件
     sum(s);  //呼叫sum(s)來計算s的總和並更新ret
     return f(ret);  //遞迴呼叫f(字串總和)
@@ -27,7 +27,7 @@ char* f(char s[11]){
 
 int main(){
     char n[11];  //儲存輸入的字串
-    while(scanf("%s",n)!=EOF){  //讀到輸入檔結尾(scanf中的n不需要加上&，因為陣列名稱本身就是位址)
+    while(scanf("%s",n)!=EOF){  //反覆讀取 (scanf中的n不需要加上&，因為陣列名稱本身就是位址)
         if(len(n)==1 && n[0]=='0') break;  //題目要求的中止輸入條件
         printf("%s\n",f(n));  //輸出f(n)的結果
     }
